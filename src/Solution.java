97293e1094f9eeaa65fbc53bcefcda51439293c9
AhmedEldeeb <88969829+Foxdanger1412@users.noreply.github.com>
2022-10-28 14:21:31 +0200

    solution of first task
    المشكلة هنا يا علي ان ف الكود اللي تحت اللي المعيدة كتباه كانت بتتطبع المصفوفة اللي هي وخداها مش اللي احنا حسبناها 
    
    "انا خدت الكود كوبي بيست من هاكر رانك"

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,40 @@
+import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 	public int[] reverse(int[] array){
-        int[] reversedArray = new int[array.length];
-        for(int i=array.length; i>0; i--)
-            reversedArray[i-1]=array[array.length-i];
-        return reversedArray;
+    
+      /*Implement your reverse method here*/
+	int l = array.length;
+	int[] op = new int[];
+	int temp = l;
+	for(int i=0; i<l; i++){
+		op[temp-1] = array[i];
+		temp -= 1;
+	}
+    
     }
-
-    public void printArray(int[] result){
-        System.out.print("[");
-        for(int i = 0; i < result.length; ++i) {
-          System.out.print(result[i]);
-          if(i != result.length - 1)
-            System.out.print(", ");
-      }
-      System.out.print("]");
-    }
-
 	public static void main(String[] args) {
-
-        //To take input array with format : [1,2,3,5]
 		Scanner sc = new Scanner(System.in);
         String sin = sc.nextLine().replaceAll("\\[|\\]", "");
-    	String[] s = sin.split(", ");
-        //To take input array with format : [1,2,3,5]
-
+    	String[] s = sin.split(", ");;
 		int[] arr = new int[s.length];
         if (s.length == 1 && s[0].isEmpty())
             arr = new int[]{};
         else {
             for(int i = 0; i < s.length; ++i)
-        	   arr[i] = Integer.parseInt(s[i]); //this class provide many methodes for integer convertion
+        	   arr[i] = Integer.parseInt(s[i]);
         }
       	int[] res = new Solution().reverse(arr);
-        new Solution().printArray(res);
-        sc.close();
+     	System.out.print("[");
+      	for(int i = 0; i < arr.length; ++i) {
+        	System.out.print(res[i]);
+            if(i != s.length - 1)
+              System.out.print(", ");
+        }
+        System.out.print("]");
     }
 }
