1139d555dbe31870e161a0fa6d2cf8c7248c75e4
AhmedEldeeb <88969829+Foxdanger1412@users.noreply.github.com>
2022-10-28 15:43:55 +0200

    solution of task3
    خد بالك عشان لو دخل انبوت فاضي الكود يعرف يتعامل معاه

--- src/Solution3.java
+++ src/Solution3.java
@@ -31,7 +31,7 @@ public class Solution {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT.            Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
         String sin = sc.nextLine().replaceAll("\\[|\\]", "");
-        String[] s = sin.split(", ");;
+        String[] s = sin.split(", ");
         int[] arr = new int[s.length];
         if (s.length == 1 && s[0].isEmpty())
             arr = new int[]{};
@@ -39,14 +39,10 @@ public class Solution {
             for(int i = 0; i < s.length; ++i)
                arr[i] = Integer.parseInt(s[i]);
         }
-         int[] res = new Solution().sumEvenOdd(arr);
-         System.out.print("[");
-          for(int i = 0; i < arr.length; ++i) {
-            System.out.print(res[i]);
-            if(i != s.length - 1)
-              System.out.print(", ");
-        }
-        System.out.print("]");
+        
+        int[] result = new Solution().sumEvenOdd(arr);
+        System.out.print("[" + result[0] + ", " + result[1] + "]");
+         
         
     }
 }
