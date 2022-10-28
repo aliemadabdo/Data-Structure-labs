40d5c2fb3731517d8779c4a69c8b1248379aa5ad
AhmedEldeeb <88969829+Foxdanger1412@users.noreply.github.com>
2022-10-28 16:38:14 +0200

    solution of task4
    هنا يمعلم انا لفيت علي المصفوفة كلها ولو العنصر مش بيساوي الرقم اللي انا عاوزه يشفت العنصر شمال ولو هو اللي انا عاوزه مش هيشفت حاجة فاهم فكل اللي مش مساوي للعنصر اللي عاوزه هيروح شمال و العنصر اللي عاوزه بالتبعية هيتحرك لليمين فاهم

--- src/Solution4.java
+++ src/Solution4.java
@@ -1,64 +1,49 @@
+import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-
-public class Solution4 {
-    
-    // public int[] getInputInt(){
-        
-    //     return arr;
-    // }
-
+public class Solution {
 	public int[] moveValue(int[] array, int value) {
-        int downCounter=1, i=0;
-        while(array.length>=downCounter+i+1){   
-            if (array[i] == value) {
-                //swaping
-                array[i] += array[array.length-downCounter];
-                array[array.length-downCounter] = array[i] - array[array.length-downCounter];
-                array[i] -= array[array.length-downCounter];
-                downCounter++;
+    	
+        int k = 0;
+        int l = array.length;
+        for(int i=0; i<l; i++){
+            if(array[i]!=value){
+                array[k++] = array[i];
             }
-            if (array[i]!=value)
-                i++;
-            }
+        }
+        while(k<l){
+            array[k++] = value;
+        }
         return array;
     }
-   
-    public void printArray(int[] result){
-        System.out.print("[");
-        for(int i = 0; i < result.length; ++i) {
-          System.out.print(result[i]);
-          if(i != result.length - 1)
-            System.out.print(", ");
-        }
-        System.out.print("]");
-    }
+    
+    
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         String sin = sc.nextLine().replaceAll("\\[|\\]", "");
-    	String[] s = sin.split(", ");
-        //To take input array with format : [1, 2, 3, 5]
-
-		int[] arr = new int[s.length];
+        String[] s = sin.split(", ");;
+        int[] arr = new int[s.length];
         if (s.length == 1 && s[0].isEmpty())
             arr = new int[]{};
         else {
             for(int i = 0; i < s.length; ++i)
-        	   arr[i] = Integer.parseInt(s[i]); //this class provide many methodes for integer convertion
-            } 
-       // sc.close();
-      //  int[] array = new Solution4().getInputInt();  //input array 
+               arr[i] = Integer.parseInt(s[i]);
+        }
+        int v = sc.nextInt();
         
-      Scanner in = new Scanner(System.in);
-      String sIn = "2";
-      if(in.hasNextLine()){
-          sIn = in.nextLine();
-        }                 
-      int movement = Integer.parseInt(sIn);
-      in.close();
-
-        int[] result = new Solution4().moveValue(arr,movement);   //process
-        new Solution4().printArray(result);                         //output
+        int[] res = new Solution().moveValue(arr, v);
+        System.out.print("[");
+          for(int i = 0; i < arr.length; ++i) {
+            System.out.print(res[i]);
+            if(i != s.length - 1)
+              System.out.print(", ");
+        }
+        System.out.print("]");
+          
+          
+        
     }
 }
