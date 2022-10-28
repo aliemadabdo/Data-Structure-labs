67ddd1ebf81367fddf38e235d31e41eec2dab7c6
AhmedEldeeb <88969829+Foxdanger1412@users.noreply.github.com>
2022-10-28 14:34:22 +0200

    solution of task 2
    خلي بالك بقي هنا وانت بتجيب الافريج لازم تحول ناتج القسمة قبل ماتخزنه لدابل الاول عشان ميقصش اي ارقام بعد العلامة و يخزن اكبر رقم ممكن

--- src/Solution2.java
+++ src/Solution2.java
@@ -1,30 +1,36 @@
+import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-public class Solution2 {
+public class Solution {
 	public double average(int[] array) {
-        double sum=0;
-        for(int i=0; i<array.length; i++){
+        int l = array.length;
+        int sum = 0;
+        for(int i=0; i<l; i++){
             sum += array[i];
         }
-        if (array.length==0) return 0;
-        else  return sum/array.length;
-       
+        double average = (double)sum / l;
+        return average;
     }
     public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
         String sin = sc.nextLine().replaceAll("\\[|\\]", "");
-    	String[] s = sin.split(", ");
-        //To take input array with format : [1,2,3,5]
-
-		int[] arr = new int[s.length];
+        String[] s = sin.split(", ");;
+        int[] arr = new int[s.length];
         if (s.length == 1 && s[0].isEmpty())
             arr = new int[]{};
         else {
             for(int i = 0; i < s.length; ++i)
-        	   arr[i] = Integer.parseInt(s[i]); //this class provide many methodes for integer convertion
-            }
-        double arrAverage = new Solution2().average(arr);
-        System.out.print(arrAverage);
-        sc.close();
+               arr[i] = Integer.parseInt(s[i]);
+        }
+         if(arr.length == 0){
+             System.out.print(0.0);
+         }else{
+             double out = new Solution().average(arr);
+             System.out.print(out);          
+         } 
     }
 }
