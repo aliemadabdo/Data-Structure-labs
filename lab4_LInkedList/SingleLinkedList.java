103b64c037004df8af2fb6859c88b62e463b5bdd
aliemadabdo <a.e.a3426@gmail.com>
2022-11-29 07:03:14 +0200

    addToIndex fn implementation,limits&error handling

--- lab4_LInkedList/SingleLinkedList.java
+++ lab4_LInkedList/SingleLinkedList.java
@@ -9,13 +9,13 @@ import javax.xml.transform.Source;
 interface ILinkedList {
 
 public void        add      (Object element                    ); 
-// public void        add      (int    index    ,  Object element );
+public void        add      (int    index    ,  Object element );
 // public Object      get      (int    index                      );
 // public void        set      (int    index    ,  Object element );
 // public void        clear    (                                  );
 // public boolean     isEmpty  (                                  );
 // public void        remove   (int    index                      );
-// public int         size     (                                  );
+ public int         size     (                                  );
 // public ILinkedList sublist  (int    fromIndex , int    toIndex );
 // public boolean     contains (Object o                          );
 }
@@ -63,6 +63,49 @@ public class SingleLinkedList implements ILinkedList {
         size++;
     }
 
+    public void add(int index, Object element){
+        Node node = new Node();
+        node.element=element;
+
+        if (head==null){
+            head = node;
+        }
+        else {
+            Node tempNode = head;               //we didnt loop with head to remain it at the start
+            Node afterNode = new Node();
+            int i = 0;
+
+            if (index==0){
+                head = node;
+                node.nextNode=tempNode;
+            }
+            else {
+                while(i<index-1) {
+                    tempNode = tempNode.nextNode;
+                    i++;
+                }
+                afterNode = tempNode.nextNode;
+                tempNode.nextNode = node;
+                node.nextNode = afterNode;
+            }
+        }
+        size++;
+    }
+
+
+    public int size (){
+        return size;
+    }
+
+    // public void checkError(int index){
+    //     try {
+    //         index
+    //     } 
+    //     catch (Exception e) {
+    //         //TODO: handle exception
+    //         System.out.print("runtime error");
+    // }
+
     public void printSinglell (SingleLinkedList mySingleLinkedList){
         int i =0;
 
@@ -109,7 +152,51 @@ public class SingleLinkedList implements ILinkedList {
                 mySingleLinkedList.add(element); 
                 mySingleLinkedList.printSinglell(mySingleLinkedList); 
             break;
-           
+            case "addToIndex" :
+                index = Integer.parseInt(sc.nextLine());
+                element = Integer.parseInt(sc.nextLine());
+                if (index<0 | index>=mySingleLinkedList.size)
+                System.out.print("Error");
+                else {
+                mySingleLinkedList.add(index, element); 
+                mySingleLinkedList.printSinglell(mySingleLinkedList);
+                }
+            break;
+            // case "get"        :
+            //     index = Integer.parseInt(sc.nextLine()); 
+            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
+            // break;
+            // case "set"        : 
+            //     index = Integer.parseInt(sc.nextLine());
+            //     element = Integer.parseInt(sc.nextLine());
+            //     mySingleLinkedList.add(element); 
+            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
+            // break;
+            // case "clear"      : 
+            //     mySingleLinkedList.add(element); 
+            //     mySingleLinkedList.printSinglell(mySingleLinkedList); 
+            // break;
+            // case "isEmpty"    :
+            //     mySingleLinkedList.add(element); 
+            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
+            // break;
+            // case "remove"     :
+            //     index = Integer.parseInt(sc.nextLine()); 
+            //     mySingleLinkedList.add(element); 
+            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
+            // break;
+            case "size"       : 
+                System.out.print(mySingleLinkedList.size()); 
+            break;
+            // case "sublist"    :
+            //     fromIndex = Integer.parseInt(sc.nextLine());
+            //     toIndex = Integer.parseInt(sc.nextLine());
+            //     mySingleLinkedList.add(element); 
+            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
+            // break;
+            // case "contains"   : 
+            //     System.out.print(mySingleLinkedList.size()); 
+            // break;
         }
     }
 }
--- lab4_LInkedList/TryCatchTesting.java
+++ lab4_LInkedList/TryCatchTesting.java
@@ -1,8 +1,12 @@
-// import java.util.LinkedList;
 
 // public class TryCatchTesting {
 //     public static void main (String[] args) {
-//         LinkedList<Integer> myLinkedList  = new LinkedList<>();
-//         System.out.print(myLinkedList);
+//         try {
+//             int x = 1/0;
+//         } 
+//         catch (Exception e) {
+//             //TODO: handle exception
+//             System.out.print("runtime error");
+//         }
 //     }
 // }
