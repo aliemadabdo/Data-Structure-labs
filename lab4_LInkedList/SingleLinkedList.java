b0f7b594173b0f1ae063467ca89d359470f53e20
aliemadabdo <a.e.a3426@gmail.com>
2022-11-29 18:57:24 +0200

    the rest of the functions

--- lab4_LInkedList/SingleLinkedList.java
+++ lab4_LInkedList/SingleLinkedList.java
@@ -10,14 +10,14 @@ interface ILinkedList {
 
 public void        add      (Object element                    ); 
 public void        add      (int    index    ,  Object element );
-// public Object      get      (int    index                      );
-// public void        set      (int    index    ,  Object element );
-// public void        clear    (                                  );
-// public boolean     isEmpty  (                                  );
-// public void        remove   (int    index                      );
- public int         size     (                                  );
-// public ILinkedList sublist  (int    fromIndex , int    toIndex );
-// public boolean     contains (Object o                          );
+public Object      get      (int    index                      );
+public void        set      (int    index    ,  Object element );
+public void        clear    (                                  );
+public boolean     isEmpty  (                                  );
+public void        remove   (int    index                      );
+public int         size     (                                  );
+public ILinkedList sublist  (int    fromIndex , int    toIndex );
+public boolean     contains (Object o                          );
 }
 
 
@@ -35,14 +35,6 @@ public class SingleLinkedList implements ILinkedList {
 
     int size =0;
     Node head;
-    
-    // public int data;
-    // public SingleLinkedList next;
-
-    // SingleLinkedList(int data){
-    //     this.data = data;
-    //     this.next = null;
-    // }
 
     public void add(Object element){
 
@@ -64,49 +56,120 @@ public class SingleLinkedList implements ILinkedList {
     }
 
     public void add(int index, Object element){
+        Node tempNode = head;               //we didnt loop with head to remain it at the start
+        Node afterNode = new Node();
         Node node = new Node();
         node.element=element;
-
-        if (head==null){
+        
+        int i = 0;
+        if (index==0){
             head = node;
+            node.nextNode=tempNode;
         }
         else {
-            Node tempNode = head;               //we didnt loop with head to remain it at the start
-            Node afterNode = new Node();
-            int i = 0;
-
-            if (index==0){
-                head = node;
-                node.nextNode=tempNode;
+            while(i<index-1) {
+                tempNode = tempNode.nextNode;
+                i++;
             }
-            else {
-                while(i<index-1) {
-                    tempNode = tempNode.nextNode;
-                    i++;
-                }
-                afterNode = tempNode.nextNode;
-                tempNode.nextNode = node;
-                node.nextNode = afterNode;
-            }
+            afterNode = tempNode.nextNode;
+            tempNode.nextNode = node;
+            node.nextNode = afterNode;
         }
         size++;
     }
 
+    public Object get(int index){
+        Node tempNode = head;               
+        int i = 0;
+            while(i<index) {
+                tempNode = tempNode.nextNode;
+                i++;
+            }
+        return tempNode.element;
+    }
 
-    public int size (){
+    public void set(int index, Object element){
+        Node tempNode = head;
+        int i = 0;
+        while(i<index) {
+            tempNode = tempNode.nextNode;
+            i++;
+        }
+        tempNode.element = element;
+    }
+
+    public void clear(){
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty(){
+        if (head==null)
+            return true;
+        else 
+        return false;
+    }
+
+    public void remove (int index){
+        Node tempNode = head;
+        int i = 0;
+        if (index == 0)
+            head = tempNode.nextNode;
+        else{
+            while(i<index-1) {
+                tempNode = tempNode.nextNode;
+                i++;
+            }
+            tempNode.nextNode=tempNode.nextNode.nextNode;
+        }
+        size--;
+    }
+
+    public int size(){
         return size;
     }
 
-    // public void checkError(int index){
-    //     try {
-    //         index
-    //     } 
-    //     catch (Exception e) {
-    //         //TODO: handle exception
-    //         System.out.print("runtime error");
-    // }
+    public ILinkedList sublist(int fromIndex, int toIndex){
+        // Node subHead;
+        SingleLinkedList  mySubSingleLinkedList  = new SingleLinkedList();
+        Node tempNode = head;
+        
+        int i = 0;
+        while(i<fromIndex){
+            tempNode = tempNode.nextNode;
+            i++;
+        }
+        head = tempNode;
+        while(i<toIndex){
+            mySubSingleLinkedList.add(tempNode);
+            tempNode = tempNode.nextNode;
+            i++;  
+        }
+        tempNode.nextNode=null;
+        return mySubSingleLinkedList;
+    }
 
-    public void printSinglell (SingleLinkedList mySingleLinkedList){
+    public boolean contains(Object o){
+        boolean flag = false;
+        Node tempNode = head;
+        for (int i=0;i<size;i++){
+            if (tempNode.element==o){
+                flag=true;
+                //break;
+            }
+            tempNode = tempNode.nextNode;
+        }
+        return flag;
+    }
+
+    public void printTorF(boolean state){
+        if (state == true)
+            System.out.print("True");
+        else if (state == false)
+        System.out.print("False");
+    }
+
+    public void printSinglell (){
         int i =0;
 
         if (head==null){
@@ -128,6 +191,28 @@ public class SingleLinkedList implements ILinkedList {
         }
     }
 
+    // public void printInterface (ILinkedList mySubSingleLinkedList){
+    //     int i =0;
+
+    //     if (head==null){
+    //         System.out.print("[]");
+    //     }
+    //     else {
+    //         Node tempNode = head;               //we didnt loop with head to remain it at the start
+
+    //         System.out.print("[");
+    //         while(tempNode.nextNode!=null) {
+    //             System.out.print(tempNode.element);
+    //             if (i<size)
+    //                 System.out.print(", ");
+    //             tempNode = tempNode.nextNode;
+    //             i++;
+    //         }
+    //         System.out.print(tempNode.element);
+    //         System.out.print("]");
+    //     }
+    // }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -135,7 +220,7 @@ public class SingleLinkedList implements ILinkedList {
     	String[] s = sin.split(", ");
         //To take input array with format : [1,2,3,5]
 
-		 SingleLinkedList  mySingleLinkedList  = new SingleLinkedList();
+		SingleLinkedList  mySingleLinkedList  = new SingleLinkedList();
         if (s.length == 1 && s[0].isEmpty())
             mySingleLinkedList = new SingleLinkedList();
         else {
@@ -150,54 +235,79 @@ public class SingleLinkedList implements ILinkedList {
             case "add"        : 
                 element = Integer.parseInt(sc.nextLine());
                 mySingleLinkedList.add(element); 
-                mySingleLinkedList.printSinglell(mySingleLinkedList); 
+                mySingleLinkedList.printSinglell(); 
             break;
+
             case "addToIndex" :
                 index = Integer.parseInt(sc.nextLine());
                 element = Integer.parseInt(sc.nextLine());
                 if (index<0 | index>=mySingleLinkedList.size)
-                System.out.print("Error");
+                    System.out.print("Error");
                 else {
-                mySingleLinkedList.add(index, element); 
-                mySingleLinkedList.printSinglell(mySingleLinkedList);
+                    mySingleLinkedList.add(index, element); 
+                    mySingleLinkedList.printSinglell();
                 }
             break;
-            // case "get"        :
-            //     index = Integer.parseInt(sc.nextLine()); 
-            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
-            // break;
-            // case "set"        : 
-            //     index = Integer.parseInt(sc.nextLine());
-            //     element = Integer.parseInt(sc.nextLine());
-            //     mySingleLinkedList.add(element); 
-            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
-            // break;
-            // case "clear"      : 
-            //     mySingleLinkedList.add(element); 
-            //     mySingleLinkedList.printSinglell(mySingleLinkedList); 
-            // break;
-            // case "isEmpty"    :
-            //     mySingleLinkedList.add(element); 
-            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
-            // break;
-            // case "remove"     :
-            //     index = Integer.parseInt(sc.nextLine()); 
-            //     mySingleLinkedList.add(element); 
-            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
-            // break;
+
+            case "get"        :
+                index = Integer.parseInt(sc.nextLine()); 
+                if (index<0 | index>=mySingleLinkedList.size)
+                    System.out.print("Error");
+                else 
+                    System.out.print(mySingleLinkedList.get(index)); 
+            break;
+
+            case "set"        : 
+                index = Integer.parseInt(sc.nextLine());
+                element = Integer.parseInt(sc.nextLine());
+                if (index<0 | index>=mySingleLinkedList.size)
+                    System.out.print("Error");
+                else {
+                    mySingleLinkedList.set(index, element); 
+                    mySingleLinkedList.printSinglell();
+                }
+            break;
+
+            case "clear"      : 
+                mySingleLinkedList.clear(); 
+                mySingleLinkedList.printSinglell(); 
+            break;
+
+            case "isEmpty"    :
+                mySingleLinkedList.printTorF(mySingleLinkedList.isEmpty()); 
+            break;
+
+            case "remove"     :
+                index = Integer.parseInt(sc.nextLine()); 
+                if (index<0 | index>=mySingleLinkedList.size)
+                    System.out.print("Error");
+                else {
+                    mySingleLinkedList.remove(index); 
+                    mySingleLinkedList.printSinglell(); 
+                }
+            break;
+
             case "size"       : 
                 System.out.print(mySingleLinkedList.size()); 
             break;
-            // case "sublist"    :
-            //     fromIndex = Integer.parseInt(sc.nextLine());
-            //     toIndex = Integer.parseInt(sc.nextLine());
-            //     mySingleLinkedList.add(element); 
-            //     mySingleLinkedList.printSinglell(mySingleLinkedList);
-            // break;
-            // case "contains"   : 
-            //     System.out.print(mySingleLinkedList.size()); 
-            // break;
+
+            case "sublist"    :
+                fromIndex = Integer.parseInt(sc.nextLine());
+                toIndex = Integer.parseInt(sc.nextLine());
+                if ( fromIndex>toIndex | fromIndex<0 | toIndex>=mySingleLinkedList.size )
+                    System.out.print("Error");
+                else {
+                    mySingleLinkedList.sublist(fromIndex, toIndex); 
+                    mySingleLinkedList.printSinglell(); 
+                }
+            break;
+
+            case "contains"   : 
+                element = Integer.parseInt(sc.nextLine());
+                mySingleLinkedList.printTorF(mySingleLinkedList.contains(element));  
+            break;
         }
+        //sc.close();
     }
 }
 
